6e5f906ff2ff29fe1d5e3cbdcd6c5bec4bd6c639
João Lenon <lenonSec7@gmail.com>
2022-03-28 10:26:02 -0300

    feat: add server and route provider

---
+++ src/Providers/HttpServerProvider.ts
@@ -0,0 +1,19 @@
+/**
+ * @athenna/http
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { Http } from 'src/Http'
+import { ServiceProvider } from '@athenna/ioc'
+
+export class HttpServerProvider extends ServiceProvider {
+  boot() {}
+
+  register() {
+    this.container.singleton('Core/Http/Server', new Http())
+  }
+}
