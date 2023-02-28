d549e6676c75a788353a4970de60299250a7afd7
João Lenon <lenonSec7@gmail.com>
2023-02-27 23:30:34 -0300

    feat(exception): export all http exceptions

---
+++ src/Exceptions/ForbiddenException.ts
@@ -0,0 +1,27 @@
+/**
+ * @athenna/http
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { HttpException } from '#src/Exceptions/HttpException'
+
+export class ForbiddenException extends HttpException {
+  /**
+   * This exception uses the 403 status code and the "E_FORBIDDEN_ERROR" code.
+   *
+   * @example
+   * ```js
+   * throw new ForbiddenException()
+   * ```
+   */
+  public constructor(
+    message = 'Forbidden request error happened.',
+    help = null,
+  ) {
+    super({ help, message, status: 403, code: 'E_FORBIDDEN_ERROR' })
+  }
+}
---
+++ src/Exceptions/PayloadTooLargeException.ts
@@ -0,0 +1,27 @@
+/**
+ * @athenna/http
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import { HttpException } from '#src/Exceptions/HttpException'
+
+export class PayloadTooLargeException extends HttpException {
+  /**
+   * This exception uses the 413 status code and the "E_PAYLOAD_TOO_LARGE_ERROR" code.
+   *
+   * @example
+   * ```js
+   * throw new PayloadTooLargeException()
+   * ```
+   */
+  public constructor(
+    message = 'Payload to large error happened.',
+    help = null,
+  ) {
+    super({ help, message, status: 413, code: 'E_PAYLOAD_TOO_LARGE_ERROR' })
+  }
+}
