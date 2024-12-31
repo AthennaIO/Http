9a5cd153e78c1cf6b64e80997adf6874c5253618
jlenon7 <lenonSec7@gmail.com>
2024-12-31 14:01:30 -0300

    feat(vite): export athenna plugin

---
+++ src/types/vite/PluginOptions.ts
@@ -0,0 +1,37 @@
+/**
+ * @athenna/http
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export interface PluginOptions {
+  /**
+   * The URL where the assets will be served. This is particularly
+   * useful if you are using a CDN to deploy your assets.
+   *
+   * @default ''
+   */
+  assetsUrl?: string
+
+  /**
+   * Files that should trigger a page reload when changed.
+   *
+   * @default ['./src/resources/views/** /*.edge']
+   */
+  reload?: string[]
+
+  /**
+   * Paths to the entrypoints files
+   */
+  entrypoints: string[]
+
+  /**
+   * Public directory where the assets will be compiled.
+   *
+   * @default 'public/assets'
+   */
+  buildDirectory?: string
+}
